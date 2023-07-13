c60c355f46902549366162deb1565a6f109fe79e
kithinjibrian <kithinjigroup@gmail.com>
2023-07-13 23:45:53 +0300

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+char str1 = "\"and that piece of art is useful\" ";
+char str2 = "- Dora Korpar, 2015-10-19\n";
+
+fprintf(stderr, str1 + str2);
+return (1);
 }
