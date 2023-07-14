3d6f7bdf9f052aed2aa85df97cb94b0d54844399
kithinjibrian <kithinjigroup@gmail.com>
2023-07-14 15:14:36 +0300

    updates

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i, j;
-
 for (i = 0; i <= 99; i++)
 {
 int tens1 = i / 10;
 int ones1 = i % 10;
-
 for (j = i; j <= 99; j++)
 {
 int tens2 = j / 10;
 int ones2 = j % 10;
-
 if (i != j)
 {
 if (i < 10)
@@ -30,9 +26,7 @@ else
 putchar('0' + tens1);
 putchar('0' + ones1);
 }
-
 putchar(' ');
-
 if (j < 10)
 {
 putchar('0');
@@ -43,12 +37,10 @@ else
 putchar('0' + tens2);
 putchar('0' + ones2);
 }
-
 putchar(',');
 putchar(' ');
 }
 }
 }
-
 return (0);
 }
