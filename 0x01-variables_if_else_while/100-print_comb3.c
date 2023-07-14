201511374baa595f26c0e8d1c2880525fb7727e1
kithinjibrian <kithinjigroup@gmail.com>
2023-07-14 14:44:32 +0300

    updates

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-int i,j;
+int i, j;
 
 for (i = 0; i < 10; i++)
 {
-    for (j = i; j < 10; j++)
-    {
-        if (i != j)
-        {
-            putchar(i + 48);
-            putchar(j + 48);
-            if ((i + 48) != '8')
-            {
-            putchar(',');
-            putchar(' ');
-            }
-        }
-    }
+for (j = i; j < 10; j++)
+{
+if (i != j)
+{
+putchar(i + 48);
+putchar(j + 48);
+if ((i + 48) != '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 putchar('\n');
 return (0);
