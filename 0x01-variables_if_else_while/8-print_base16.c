d44fada2f6f8ba049b66b6cd81a6674fb02975f2
kithinjibrian <kithinjigroup@gmail.com>
2023-07-14 13:38:42 +0300

    updates

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d;
+
+for (d = 10; d <= 15; d++)
+{
+putchar(d + 87);
+}
+putchar('\n');
+return (0);
+}
