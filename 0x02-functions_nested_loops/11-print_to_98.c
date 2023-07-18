dcafd2d2b95f93554d19b87d2d10a4e585c66b6b
kithinjibrian <kithinjigroup@gmail.com>
2023-07-19 00:52:07 +0300

    2-print_alphabet

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,32 +11,23 @@ void print_to_98(int lb)
 	{
 		for (lb; lb >= 98; lb--)
 		{
-			printf("%d", lb);
+			_putchar(lb);
 			if(lb != 98)
 			{
-				printf(", ");
+				_putchar(",");
+				_putchar(" ");
 			}
 		}
 	}
 	else {
 		for (lb; lb <= 98; lb++)
 		{
-			printf("%d", lb);
+			_putchar(lb);
 			if(lb != 98)
 			{
-				printf(", ");
+				_putchar(", ");
 			}
 		}
 	}
-	printf("\n");
-}
-
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
+	_putchar("\n");
 }
