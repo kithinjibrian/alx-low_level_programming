f9c827aed3b285853faf9c50e7247008652e818b
kithinjibrian <kithinjigroup@gmail.com>
2023-07-18 11:03:30 +0300

    putchar

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	for (int h = 0; h < 24; h++)
+	{
+		for (int m = 0; m < 60; m++)
+		{
+			printf("%02d:%02d\n",h,m);
+		}
+	}
+}
+
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
