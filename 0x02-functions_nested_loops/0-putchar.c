443f55425472995f72c4f80ba03dffe301fd1544
kithinjibrian <kithinjigroup@gmail.com>
2023-07-18 23:27:35 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,9 @@
 /**
  *main - entry point
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int main(void)
 {
 	printf("_putchar\n");
