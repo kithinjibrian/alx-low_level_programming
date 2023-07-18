1d352f4f5aebc2913df7799c8fd27cffc0339057
kithinjibrian <kithinjigroup@gmail.com>
2023-07-19 00:10:37 +0300

    2-print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+/**
   * print_alphabet_x10 - repeats the print_alphabet 10 times.
   * Return: Nothing.
   */
