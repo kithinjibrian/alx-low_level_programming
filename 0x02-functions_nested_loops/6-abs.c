e49e65fbddebeb246adb6434af7f3279af4340c4
kithinjibrian <kithinjigroup@gmail.com>
2023-07-19 00:27:47 +0300

    2-print_alphabet

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- *
- *
- *
+ * _abs - calculate the absolute value of the number from zero.
+ * @i: the integer to be converted.
+ * Return: absolute value of the input number.
  */
 
 int _abs(int i)
 {
 	return (abs(i));
-}
-
-int main(void)
-{
-	printf("%d\n", _abs(-1));
-	printf("%d\n", _abs(0));
-	printf("%d\n", _abs(1));
-	printf("%d\n", _abs(-98));
-	return (0);
 }
