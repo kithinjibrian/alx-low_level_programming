bbcf9e552446e48e88dd8de2a16ac6a4f9f1d7c2
kithinjibrian <kithinjigroup@gmail.com>
2023-07-20 10:51:11 +0300

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _isupper - function to determine if a character is uppercase
+ * @ch: character
+ * Return: 1 if ch is uppercase
+ */
+
+int _isupper(char ch)
+{
+	return (isupper(ch) ? 1 : 0);
+}
