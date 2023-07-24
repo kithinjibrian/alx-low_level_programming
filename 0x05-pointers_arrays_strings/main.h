a6e70ee19439ee84b5f86ab4a67bd071fa812942
kithinjibrian <kithinjigroup@gmail.com>
2023-07-25 00:04:25 +0300

    updates

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,9 @@
 int _putchar(char ch);
 void reset_to_98(int *m);
+void swap_init(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *s);
+void print_array(int *a, int n);
