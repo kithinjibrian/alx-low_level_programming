6661d957b6e9cf0046b6d868547d5cf462ce222d
kithinji brian <96344063+kithinjibrian@users.noreply.github.com>
2023-08-31 09:52:24 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
-	unsigned int i=0;
-	int c;
-	unsigned int len;
+	unsigned int total, power;
+	int len;
 
-	len = strlen(b);
-
-	for (c = len-1; c >= 0; c--)
-	{
-		if (b[c] != '0' && b[c] != '1')
+	if (b == NULL)
 		return (0);
-		if (b[c] == '1')
-		{
-		i += k;
-		}
-	k *= 2;
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
 
-	return (i);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
