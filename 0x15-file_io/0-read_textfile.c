593becc0e878c7ed99036f267164e7857a57de9e
kithinjibrian <kithinjigroup@gmail.com>
2023-09-08 23:48:34 +0300

    update read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,38 +12,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (-1);
-
-	buffer = (char *)malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
+		return (0);
 
-	r = read(fd, buffer, letters);
-	if (r == -1)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		close(fd);
-		return (-1);
+		return (0);
 	}
 
-	w = write(STDOUT_FILENO, buffer, r);
 	free(buffer);
-	close(fd);
-
-	if (w == -1 || w != r)
-		return (-1);
+	close(o);
 
 	return (w);
 }
